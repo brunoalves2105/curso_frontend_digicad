3cf9706ff3a99afd46e1155010be4d55b5cb0d9e
brunoalves2105 <bruno_2105@outlook.com>
2020-01-24 21:55:45 -0300

    inclusão do exercicio de javascript

---
+++ usando javascript/js/comandosbasicos.js
@@ -0,0 +1,85 @@
+// comandos basicos do javascript
+
+/*
+kjgkjs
+*/
+/*
+let meuTexto = 'exemplo de string';
+var meuNumero = 10.5;
+
+console.log(typeof 10);
+console.log("meu numero:"+ (meuNumero + 13));
+
+console.log(typeof "exemplo \"de uma\" string");
+console.log("Meu texto: "+ meuTexto);
+console.log("Meu texto: "+ meuTexto + " \ne meu \"numero:\" "+ (meuNumero + 13)); */
+
+/* var _textogeral = "bruno";
+console.log(_textogeral[2]);
+console.log(_textogeral [_textogeral.length--]); */
+/*
+let var1 = "eu sou uma";
+let var2 = 5;
+let var3 = 10;
+let var4 = 20;
+let var5 = 35;
+let var6 = 56;
+
+var1 += " string!";
+var2 *= 5;
+var3 -= 7;
+var4 %= 2;
+var5 /= 5;
+var6 **= 2;
+
+
+console.log('var1:'+ var1);
+console.log('var2:'+ var2);
+console.log('var3:'+ var3);
+console.log('var4:'+ var4);
+console.log('var5:'+ var5);
+console.log('var6:'+ var6); */
+/*
+var vetor = [5,10,11,7];
+console.log(vetor.length);
+console.log(vetor[3]);
+vetor.push(2);
+console.log(vetor[2]);
+console.log(vetor.length);
+console.log(vetor[vetor.length-1]);
+vetor.shift(12);
+console.log(vetor.length);
+console.log(vetor[0]);*/
+/*
+let notas = [
+    ["html",10],
+    ["css",10],
+    ["javascript",10]
+]
+
+console.log(notas[1][0]); */
+
+
+/*
+var idade = 65;
+    if(idade >= 18){
+        console.log("vota");
+    } else if (idade < 18 || idade >= 16 || idade >= 60) {
+        console.log("voto opcional");
+    } else{
+        console.log("não vota");
+    } */
+    
+/*
+var idade = 18;
+switch (idade){
+        case 18 : alert("valor igual a 18");
+        break;
+        case 20:  console.log("valor igual a 20");
+        break;
+        case 3:  console.log("valor igual a 3");;
+        default: console.log("nenhum valor encontrado");
+} */
+
+
+
